dfc54207c152bc79c53c8dbb2f53e0922599a62c
HeviDevi <devon.r.ross@gmail.com>
2024-07-18 13:44:20 -0400

    added example .env variables

---
+++ README.md
@@ -0,0 +1,9 @@
+Example of .env 
+
+CLOUD_NAME= Cloudinary Cloud name
+API_KEY= it's API key
+API_SECRET= it's secret key 
+
+MONGO_URI= Your mongo uri
+MONGO_DB_NAME= the mongo DB name
+MONGO_HOST= host name
--- utils/multer.js
+++ utils/multer.js
@@ -1,22 +1,24 @@
 const multer = require('multer');
 
 
-module.exports = multer({
-    storage : multer.diskStorage({}),
-    limits: {
-        fileSize: 1024 * 1024 * 5
-    },
-     fileFilter (req, file, cb) {
 
-        // The function should call `cb` with a boolean
-        // to indicate if the file should be accepted
-      if(!file.mimetype.match(/jpeg|jpg|png|gif$/i)){
-        cb(new Error('File is not supported'), false)
-        return;
+module.exports = multer({
+  // Configuring storage for the uploaded files. Here, we use the diskStorage engine which stores files on the disk.
+  storage: multer.diskStorage({}),
+  //Here, we limit the file size to 5MB.
+  limits: {
+      fileSize: 1024 * 1024 * 5 // 1024 * 1024 represents 1MB. So, 1024 * 1024 * 5 is 5MB.
+  },
+  // Defining a file filter function to control which files should be uploaded.
+  fileFilter(req, file, cb) {
+      // Checking the file's mimetype to ensure it matches specific formats (jpeg, jpg, png, gif).
+      // The mimetype is used to determine the file's type.
+      if (!file.mimetype.match(/jpeg|jpg|png|gif$/i)) {
+          // If the file's mimetype does not match the allowed types, pass an error to the callback.
+          cb(new Error('File is not supported'), false);
+          return;
       }
-      
-        // To accept the file pass `true`, like so:
-        cb(null, true)
-      
-      }
-    });
+      // If the file is of an allowed type, pass `true` to the callback indicating the file should be accepted.
+      cb(null, true);
+  }
+});
